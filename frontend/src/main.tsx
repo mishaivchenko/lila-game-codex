a960542276a054ed9a3d6c1696535b4f15a9d4fb
mykhailivchenko <mivchenko@exadel.com>
2026-02-26 14:47:18 +0200

    fix(theme): remove global deep-mode css override from app bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,20 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { AppRouter } from './app/router';
 import { GameProvider } from './context/GameContext';
 import { AppErrorBoundary } from './components/AppErrorBoundary';
-import { DeepModeThemeProvider } from './features/deep-mode';
 import { BoardThemeProvider } from './theme';
 import './styles/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppErrorBoundary>
-      <DeepModeThemeProvider>
-        <BoardThemeProvider>
-          <GameProvider>
-            <AppRouter />
-          </GameProvider>
-        </BoardThemeProvider>
-      </DeepModeThemeProvider>
+      <BoardThemeProvider>
+        <GameProvider>
+          <AppRouter />
+        </GameProvider>
+      </BoardThemeProvider>
     </AppErrorBoundary>
   </React.StrictMode>,
 );
